ae16ce45f4ba633d5d7d51ef317a456ba140daf6
Pradumna Mishra <104405973+gitpradumna@users.noreply.github.com>
2024-05-08 20:31:00 +0530

    Changes

---
+++ qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"663b935d1d712a002333a6ed",
+				"663b935d1d712a002333a6f0"
+			]
+		},
+		"663b935d1d712a002333a6ed": {
+			"id": "663b935d1d712a002333a6ed",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"663b935d1d712a002333a6f0": {
+			"id": "663b935d1d712a002333a6f0",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "gatsby",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
